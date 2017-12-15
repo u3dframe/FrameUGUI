a5f1806545bd3842a765b4a8e082d8a6edde0972
canyon <gongyanghui1986@163.com>
2017-12-15 10:28:14 +0800

    添加UGUIEventSystem

---
+++ Assets/Scripts/Core/Game/UGUIEventSystem.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(EventSystem))]
+public class UGUIEventSystem : MonoBehaviour {
+
+	bool _isCheck = false;
+	bool _isAppQuit = false;
+	EventSystem _curr;
+
+	// Use this for initialization
+	void Start () {
+		_curr = gameObject.GetComponent<EventSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!_isCheck)
+			return;
+
+		if (EventSystem.current == null) {
+			EventSystem.current = _curr;
+		}
+	}
+
+	void OnApplicationQuit(){
+		this._isAppQuit = true;
+	}
+
+	void OnDestroy(){
+		if (this._isAppQuit)
+			return;
+
+		_instance = null;
+		instance.Init (true);
+	}
+
+	public void Init(bool isCheck){
+		this._isCheck = isCheck;
+		this._isAppQuit = false;
+	}
+
+	static UGUIEventSystem _instance;
+	static public UGUIEventSystem instance{
+		get{
+			if (_instance == null) {
+				GameObject _gobj = new GameObject ("EventSystem");
+				_instance = _gobj.AddComponent<UGUIEventSystem> ();
+				GameObject.DontDestroyOnLoad (_gobj);
+			}
+			return _instance;
+		}
+	}
+}
--- Assets/Scripts/Demo.cs
+++ Assets/Scripts/Demo.cs
@@ -6,16 +6,20 @@ public class Demo : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Invoke ("FuncInvoke", 1);
+		UGUIEventSystem.instance.Init (true);
+		Invoke ("FuncInvoke",5);
 	}
 	
 	// Update is called once per frame
 	void FuncInvoke () {
-		Scene s = SceneManager.GetActiveScene ();
-		if (s.name == "Launcher") {
-			SceneManager.LoadScene (1);
-		} else {
-			SceneManager.LoadScene (0);
-		}
+		Debug.Log ("== FuncInvoke ==");
+		GameObject.Destroy (UGUIEventSystem.instance.gameObject);
+
+//		Scene s = SceneManager.GetActiveScene ();
+//		if (s.name == "Launcher") {
+//			SceneManager.LoadScene (1);
+//		} else {
+//			SceneManager.LoadScene (0);
+//		}
 	}
 }
